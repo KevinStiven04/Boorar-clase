517dcaa1c19efec040d68e5be8508e44f83ce2ef
KevinStiven04 <kevinmontoya0614@gmail.com>
2026-02-05 11:40:03 -0500

    feat: Creacion de main con funcion suma

---
+++ main.py
@@ -0,0 +1,16 @@
+def sumar_dos_numeros(num1, num2) -> int:
+    """
+    Suma dos números y devuelve el resultado.
+
+    Args:
+        num1 (int | float): Primer número a sumar.
+        num2 (int | float): Segundo número a sumar.
+
+    Returns:
+        int | float: Resultado de la suma de num1 y num2.
+
+    """
+    return num1 + num2
+
+
+print(sumar_dos_numeros(1, 3))
